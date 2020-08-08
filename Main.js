bb59bf117ae768b766c631183ea83e58b79cbc99
hkl9 <hkl9@kent.ac.uk>
2020-08-08 01:39:46 +0100

    Update Main.js
    Beautifying...

--- Main.js
+++ Main.js
@@ -1,27 +1,27 @@
 function fetch() {
 //Retrieving all entries 
-    row=0;
+	row=0;
 	window.scroll({top: 100000, behavior: 'smooth'});
-    setTimeout(function() {
-	    row = document.querySelectorAll('.mat-row').length;
-        if(row==rowCount){
+	setTimeout(function() {
+		row = document.querySelectorAll('.mat-row').length;
+        	if(row==rowCount){
 		//if count keeps updating, mean there's more entry
 		//if not, means reaches the bottom of the file
 		//then start downloading
-		if(flag==true){
-			flag=false;
-		    console.log("final",row,rowCount);
-		scrape();
-		}
-		    return;
-	    }else{
-	    	console.log("pos",row,rowCount)
-		    rowCount = row;
+			if(flag==true){
+				flag=false;
+				console.log("final",row,rowCount);
+				scrape();
+			}
+			return;
+		}else{
+			console.log("pos",row,rowCount)
+			rowCount = row;
 			download();
-            return;
-	    };
-    }, "1000");
-    return;
+			return;
+		};
+	}, "1000");
+	return;
 }
 
 
@@ -30,19 +30,17 @@ function download() {
 	//click the entry and look up the element
 	//click the link to start downloading
 	document.querySelectorAll('.mat-row').forEach(item => {
-    	setTimeout(function() {
-		item.click()
-			
-		document.querySelectorAll('.c5e-external-link').forEach(item => {  
-            	var opened = window.open(item.getAttribute("href"))
-            	console.log(item.textContent);
-            
-		setTimeout(function() {
+    		setTimeout(function() {
+			item.click()	
+			document.querySelectorAll('.c5e-external-link').forEach(item => {  
+            			var opened = window.open(item.getAttribute("href"))
+            			console.log(item.textContent);
+				setTimeout(function() {
 					console.log("Closing");
 					opened.close()
 				}, "8000");
             
-		});
+			});
 		}, "1000");
 	})
 }
